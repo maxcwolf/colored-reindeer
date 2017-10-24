ad4eb9c8ba25a09d6180f26a1265b00dae83c102
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-24 09:40:44 -0500

    drastically simplified the factory into a single for loop that just uses the indeces of the name and color arrays to populate the objects to be placed in a cointaining array.

--- colored-reindeer.js
+++ colored-reindeer.js
@@ -1,54 +1,67 @@
 const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-const reindeerNameBuilder = function* () {
-
-    // Write a for loop that looks at each reindeer
-    let counter = 0
-    
-        while (counter < reindeer.length) {
-            yield currentName = reindeer[counter];
-            counter += 1; // Increment the counter
-        }
-}
-//creating instance of reindeer name factory
-const reindeerNameFactory = reindeerNameBuilder();
-
-
-//creating color factory function
 const reindeerColor = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
-const reindeerColorBuilder = function* () {
 
-    // Write a for loop that looks at each reindeer
-    let counter = 0
+// //NAME
+// //reindeer name generator
+// const reindeerNameBuilder = function* () {
+
+//     // Write a for loop that looks at each reindeer
+//     let counter = 0
     
-        while (counter < reindeerColor.length) {
-            yield currentName = reindeerColor[counter];
-            counter += 1; // Increment the counter
-        }
-}
-//creating instance of reindeer color factory
-const reindeerColorFactory = reindeerColorBuilder();
+//         while (counter < reindeer.length) {
+//             yield currentName = reindeer[counter];
+//             counter += 1; // Increment the counter
+//         }
+// }
+// //creating instance of reindeer name generator
+// const reindeerNameFactory = reindeerNameBuilder();
+
+
+// //COLOR
+// //creating color generator
+// const reindeerColorBuilder = function* () {
+
+//     // Write a for loop that looks at each reindeer
+//     let counter = 0
+    
+//         while (counter < reindeerColor.length) {
+//             yield currentName = reindeerColor[counter];
+//             counter += 1; // Increment the counter
+//         }
+// }
+// //creating instance of reindeer color generator
+// const reindeerColorFactory = reindeerColorBuilder();
 
 
 
+// //FACTORY
+// // Invoke factory function to create reindeer object
+// const reindeerFactory = function (name, color) {
+//     return Object.create(null, {
+//         'name' : {value : reindeerNameFactory.next().value, enumerable:true},
+//         'color' : {value : reindeerColorFactory.next().value, enumerable:true}
+//     })
+// }
 
-// Invoke factory function to create reindeer object
-const reindeerFactory = function (name, color) {
-    return Object.create(null, {
-        'name' : {value : reindeerNameFactory.next().value, enumerable:true},
-        'color' : {value : reindeerColorFactory.next().value, enumerable:true}
-    })
-}
 
+// //creating dynamic variables to hold each object created by the variables above
+// for (i = 0; i < reindeer.length; i++) {
+//     window['reindeer' + (i + 1)] = reindeerFactory(i);
+// }
 
-//creating dynamic variables to hold each object created by the variables above
-for (i = 0; i < reindeer.length; i++) {
-    window['reindeer' + (i + 1)] = reindeerFactory(i);
-}
+// let reindeers = [];
+
+// reindeers.push(reindeer1, reindeer2, reindeer3, reindeer4, reindeer5, reindeer6, reindeer7, reindeer8)
+
 
 let reindeers = [];
 
-reindeers.push(reindeer1, reindeer2, reindeer3, reindeer4, reindeer5, reindeer6, reindeer7, reindeer8)
-
+for (i = 0; i < reindeer.length; i++)  {
+    reindeers.push(Object.create(null, {
+        'name' : {value : reindeer[i], enumerable : true},
+        'color' : {value : reindeerColor[i], enumerable : true}
+    }))
+}
 console.log(reindeers);
 
 
