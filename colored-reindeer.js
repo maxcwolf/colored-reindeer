63ce066c6d5aa0f4d696bf36493c6be5a502d7fc
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-23 21:01:52 -0500

    first 2 steps done in a kind of hacky way using global variables. still need to insert the array into the dom."

---
+++ colored-reindeer.js
@@ -0,0 +1,54 @@
+const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+const reindeerNameBuilder = function* () {
+
+    // Write a for loop that looks at each reindeer
+    let counter = 0
+    
+        while (counter < reindeer.length) {
+            yield currentName = reindeer[counter];
+            counter += 1; // Increment the counter
+        }
+}
+//creating instance of reindeer name factory
+const reindeerNameFactory = reindeerNameBuilder();
+
+
+//creating color factory function
+const reindeerColor = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
+const reindeerColorBuilder = function* () {
+
+    // Write a for loop that looks at each reindeer
+    let counter = 0
+    
+        while (counter < reindeerColor.length) {
+            yield currentName = reindeerColor[counter];
+            counter += 1; // Increment the counter
+        }
+}
+//creating instance of reindeer color factory
+const reindeerColorFactory = reindeerColorBuilder();
+
+
+
+
+// Invoke factory function to create reindeer object
+const reindeerFactory = function (name, color) {
+    return Object.create(null, {
+        'name' : {value : reindeerNameFactory.next().value, enumerable:true},
+        'color' : {value : reindeerColorFactory.next().value, enumerable:true}
+    })
+}
+
+
+//creating dynamic variables to hold each object created by the variables above
+for (i = 0; i < reindeer.length; i++) {
+    window['reindeer' + (i + 1)] = reindeerFactory(i);
+}
+
+let reindeers = [];
+
+reindeers.push(reindeer1, reindeer2, reindeer3, reindeer4, reindeer5, reindeer6, reindeer7, reindeer8)
+
+console.log(reindeers);
+
+
